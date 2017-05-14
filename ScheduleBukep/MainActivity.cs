361fb0d2223696b914f102c2a649a06bc2db9e1f
Nikita Kamyshenko <hnkntoc@gmail.com>
2017-05-14 19:46:27 +0400

    Добавлен ArrayAdapter для Spinner

--- ScheduleBukep/MainActivity.cs
+++ ScheduleBukep/MainActivity.cs
@@ -1,16 +1,26 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace ScheduleBukep
 {
     [Activity(Label = "ScheduleBukep", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+
+            string[] data = { "Item_1", "Item_2", "Item_3", "Item_4" };
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleSpinnerItem, data);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinnerFaculty);
+            spinner.Adapter = adapter;
         }
     }
 }
