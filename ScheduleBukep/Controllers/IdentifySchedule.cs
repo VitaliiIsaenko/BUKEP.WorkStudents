fd4e747a3079691dfada2eb3c03d4ed285056855
Nikita Kamyshenko <hnkntoc@gmail.com>
2017-05-18 20:06:16 +0400

    Удалил FacultyWrapper

--- ScheduleBukep/Controllers/IdentifySchedule.cs
+++ ScheduleBukep/Controllers/IdentifySchedule.cs
@@ -16,37 +16,7 @@ namespace Bukep.Sheduler.Controllers
         public void Update()
         {
             List<Faculty> faculties = FacadeAPI.GetFaculties();           
-            view.ShowFaculty(WrappingFaculty(faculties));
-        }
-
-        private List<Faculty> WrappingFaculty(List<Faculty> faculties)
-        {
-            List<Faculty> facultyWrappers = new List<Faculty>();
-            foreach (var faculty in faculties)
-            {
-                facultyWrappers.Add(new FacultyWrapper(faculty));
-            }
-            return facultyWrappers;
-        }
-    }
-
-    /// <summary>
-    /// Используется для получения из ToString() имени Faculty.
-    /// Нужно для ArrayAdapter 
-    /// так как ArrayAdapter использует ToString() для отображения Faculty на View.
-    /// </summary>
-    class FacultyWrapper : Faculty
-    {
-        private Faculty faculty;
-
-        public FacultyWrapper(Faculty faculty)
-        {
-            this.faculty = faculty;
-        } 
-
-        public override string ToString()
-        {
-            return faculty.Name;
+            view.ShowFaculty(faculties);
         }
     }
 }
