a4a873e40684f8f63b681bdcc43ef8ae0087d203
Nikita Kamyshenko <hnkntoc@gmail.com>
2017-05-22 20:35:51 +0400

    Создание card_lesson_viwe

--- ScheduleBukep/Resources/Resource.Designer.cs
+++ ScheduleBukep/Resources/Resource.Designer.cs
@@ -61,7 +61,16 @@ namespace Bukep.Sheduler
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int gps = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int green_line = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int teacher = 2130837507;
 			
 			static Drawable()
 			{
@@ -76,41 +85,71 @@ namespace Bukep.Sheduler
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060009
-			public const int buttoneShow = 2131099657;
-			
-			// aapt resource value: 0x7f060000
-			public const int faculty = 2131099648;
-			
-			// aapt resource value: 0x7f060006
-			public const int group = 2131099654;
+			// aapt resource value: 0x7f060013
+			public const int buttoneShow = 2131099667;
 			
 			// aapt resource value: 0x7f06000a
-			public const int relativeLayout1 = 2131099658;
+			public const int faculty = 2131099658;
 			
-			// aapt resource value: 0x7f060002
-			public const int specialty = 2131099650;
-			
-			// aapt resource value: 0x7f060005
-			public const int spinnerCourse = 2131099653;
-			
-			// aapt resource value: 0x7f060001
-			public const int spinnerFaculty = 2131099649;
+			// aapt resource value: 0x7f060010
+			public const int group = 2131099664;
 			
 			// aapt resource value: 0x7f060007
-			public const int spinnerGroup = 2131099655;
+			public const int imageLocation = 2131099655;
 			
-			// aapt resource value: 0x7f060003
-			public const int spinnerSpecialty = 2131099651;
+			// aapt resource value: 0x7f060006
+			public const int imageTeacher = 2131099654;
+			
+			// aapt resource value: 0x7f060005
+			public const int nameLesson = 2131099653;
+			
+			// aapt resource value: 0x7f060009
+			public const int nameLocation = 2131099657;
 			
 			// aapt resource value: 0x7f060008
-			public const int tableRow1 = 2131099656;
+			public const int nameTeacher = 2131099656;
+			
+			// aapt resource value: 0x7f060001
+			public const int number = 2131099649;
+			
+			// aapt resource value: 0x7f060014
+			public const int relativeLayout1 = 2131099668;
+			
+			// aapt resource value: 0x7f06000c
+			public const int specialty = 2131099660;
+			
+			// aapt resource value: 0x7f06000f
+			public const int spinnerCourse = 2131099663;
 			
 			// aapt resource value: 0x7f06000b
-			public const int textView2 = 2131099659;
+			public const int spinnerFaculty = 2131099659;
+			
+			// aapt resource value: 0x7f060011
+			public const int spinnerGroup = 2131099665;
+			
+			// aapt resource value: 0x7f06000d
+			public const int spinnerSpecialty = 2131099661;
+			
+			// aapt resource value: 0x7f060012
+			public const int tableRow1 = 2131099666;
+			
+			// aapt resource value: 0x7f060015
+			public const int textView2 = 2131099669;
+			
+			// aapt resource value: 0x7f060002
+			public const int textView3 = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int timeEnd = 2131099651;
+			
+			// aapt resource value: 0x7f060000
+			public const int timeStart = 2131099648;
 			
 			// aapt resource value: 0x7f060004
-			public const int сourse = 2131099652;
+			public const int typeLesson = 2131099652;
+			
+			// aapt resource value: 0x7f06000e
+			public const int сourse = 2131099662;
 			
 			static Id()
 			{
@@ -126,10 +165,13 @@ namespace Bukep.Sheduler
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int IdentifyScheduleLayout = 2130903040;
+			public const int card_lesson_viwe = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int ScheduleLayout = 2130903041;
+			public const int IdentifyScheduleLayout = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int ScheduleLayout = 2130903042;
 			
 			static Layout()
 			{
