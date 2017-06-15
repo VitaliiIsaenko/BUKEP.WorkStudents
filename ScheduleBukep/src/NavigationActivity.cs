ef826e91db683dfb2d60830a593f424c8fe78ce4
Nikita Kamyshenko <hnkntoc@gmail.com>
2017-06-15 22:56:31 +0300

    Добавлен NavigationActivity

---
+++ ScheduleBukep/src/NavigationActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.App;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Widget;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace Bukep.Sheduler
+{
+    /// <summary>
+    /// Используется для работы с общей логикой навигации в Activity.
+    /// </summary>
+    public class NavigationActivity : AppCompatActivity
+    {
+        /// <summary>
+        /// Инициализация всех навигационных View. 
+        /// </summary>
+        protected void InitNavigationView()
+        {
+            InitToolbar();
+            InitBottomNavigationView();
+        }
+
+        protected virtual void InitBottomNavigationView()
+        {
+            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+            if (bottomNavigation == null)
+            {
+                throw new Exception("Failed execute InitBottomNavigationView(). Check BottomNavigationView in Layout.");
+            }
+            bottomNavigation.NavigationItemSelected += ClickBottomNavigation;
+        }
+
+        private void ClickBottomNavigation(object sender,
+            BottomNavigationView.NavigationItemSelectedEventArgs args)
+        {
+            var itemName = "";
+            switch (args.Item.ItemId)
+            {
+                case Resource.Id.menu_list_task:
+                    itemName = "menu_list_task";
+                    break;
+                case Resource.Id.menu_teachers:
+                    itemName = "menu_teachers";
+                    break;
+                case Resource.Id.menu_favorites:
+                    itemName = "menu_favorites";
+                    break;
+            }
+            Toast.MakeText(this, "Click "+ itemName, ToastLength.Long).Show();
+        }
+
+        protected virtual void InitToolbar()
+        {
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.tool_bar);
+            if (toolbar == null)
+            {
+                throw new Exception("Failed execute InitToolbar(). Check Toolbar in Layout.");
+            }
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = GetString(Resource.String.ApplicationName);
+        }
+    }
+}
